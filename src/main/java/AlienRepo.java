dd7be02d90a314f7ac8fdb37901c9dc9e6f3adcd
Shoaib Ansari <66304849+shoaib749@users.noreply.github.com>
2023-05-16 12:43:42 +0530

    Final Commit

---
+++ src/main/java/AlienRepo.java
@@ -0,0 +1,9 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.unicon.springdatarest.model.Alien;
+
+@RepositoryRestResource(collectionResourceRel="aliens",path="aliens")
+public interface AlienRepo extends JpaRepository<Alien,Integer> {
+	
+}
